2cbe67ef1cc6fc47f87a9d851509b14f860f197d
j-camposar <31163773+j-camposar@users.noreply.github.com>
2025-11-12 23:57:02 -0300

    cambios en seguridad cors

--- server.js
+++ server.js
@@ -12,10 +12,16 @@ const app = express();
 // ⚙️ Configuración general
 // ----------------------------------------------------------
 
-app.use(cors({
-  origin: 'https://yn8csy-3000.csb.app', // Cambiar por dominio real del frontend
-  credentials: true,
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 
 // Variables de entorno
